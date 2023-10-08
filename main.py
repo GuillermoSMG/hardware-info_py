45c71463b46165b070a5092593b5878b0a836a6b
Guillermo San Martín <guille.seba0802@gmail.com>
2023-10-08 12:28:37 -0300

    hardware info

---
+++ main.py
@@ -0,0 +1,14 @@
+import platform
+import psutil
+import cpuinfo
+
+print(f'Architecture: {platform.architecture()}')
+print(f'Network Name: {platform.node()}')
+print(f'Operating System: {platform.platform()}')
+
+print(f'Processor: {platform.processor()}')
+
+my_cpuinfo = cpuinfo.get_cpu_info()
+print(f'Full CPU Name: {my_cpuinfo["brand_raw"]}')
+
+print(f'Total RAM: {psutil.virtual_memory().total / 1024 / 1024 / 1024:.2f}GB')
